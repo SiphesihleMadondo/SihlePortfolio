ffc59d584b4223db20c1908a23460b5188987b9d
Siphesihle <madondosphesihles@gmail.com>
2024-04-03 12:01:40 +0200

    first batch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
+import { HearderComponent } from './hearder/hearder.component';
+import { NavComponent } from './nav/nav.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule,HearderComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'SihlePortfolio';
+  title = 'Siphesihle Portfolio';
 }
